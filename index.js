c11f0233535786d42f2cedb631fedf378ca2e7fe
denyuneedtoknow <denyuneedtoknow@gmail.com>
2022-04-08 21:45:35 +0300

    some

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const elName =pair[0]
 const elValue=pair[1]
 const elData={[elName]:elValue}
 data.push(elData)
-const dataString = document.createElement("li");
-dataString.textContent = `${elName}=${elValue}`
-refs.dataSheet.append(dataString)
-console.log(data);
+// const dataString = document.createElement("li");
+// dataString.textContent = `${elName}=${elValue}`
+// refs.dataSheet.append(dataString)
+console.log(data[0]);
+const markup = data
+  .map((dataElement) => {
+    // console.log(Object.keys(dataElement));  
+    return `<li class="list-item">${Object.keys(dataElement)}=${Object.value(dataElement)}</li>`})
+  .join("");
+  refs.dataSheet.innerHTML = markup;
+
 }
+
+
+
 
 const addingData=(e) =>{
     e.preventDefault();
